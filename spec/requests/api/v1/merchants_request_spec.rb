3a396e7ffa427505cdcbc1b37b54cbc869b3223d
Robert Heath <robert.b.heathii@gmail.com>
2021-02-16 16:13:04 -0600

    Update tests

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -19,21 +19,21 @@ describe 'Merchants API' do
       expect(merchant).to have_key(:name)
       expect(merchant[:name]).to be_a(String)
     end
+  end
 
-    it 'returns one merchant by its id' do
-      id = create(:merchant).id
+  it 'returns one merchant by its id' do
+    id = create(:merchant).id
 
-      get "/api/v1/merchants/#{id}"
+    get "/api/v1/merchants/#{id}"
 
-      merchant = JSON.parse(response.body, symbolize_names: true)
+    merchant = JSON.parse(response.body, symbolize_names: true)
 
-      expect(response).to be_successful
+    expect(response).to be_successful
 
-      expect(merchant).to have_key(:id)
-      expect(merchant[:id]).to eq(id)
+    expect(merchant).to have_key(:id)
+    expect(merchant[:id]).to eq(id)
 
-      expect(merchant).to have_key(:name)
-      expect(merchant[:name]).to be_a(String)
-    end
+    expect(merchant).to have_key(:name)
+    expect(merchant[:name]).to be_a(String)
   end
 end
