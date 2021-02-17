e91caf43dbf785bc2d6624043f13fbec01e1c6d4
Robert Heath <robert.b.heathii@gmail.com>
2021-02-17 16:57:17 -0600

    Add serializer and update controller

--- app/controllers/api/v1/merchant_items_controller.rb
+++ app/controllers/api/v1/merchant_items_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::MerchantItemsController < ApplicationController
   def index
-    render json: Merchant.find(params[:merchant_id]).items
+    render json: MerchantItemsSerializer.new(Merchant.find(params[:merchant_id]).items)
   end
 end
